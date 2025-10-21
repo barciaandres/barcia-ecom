53d751e39eb4934445d8aa388a9c8a5b1c3a786c
Andrés Barcia <barciaandres@gmail.com>
2025-10-21 00:07:07 -0300

    corrijo logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import './NavBar.css'
 import CartWidget from './CartWidget.jsx'
+import logo from '../assets/logo_barcia.png'
 const NavBar = () => {
     return (
         <header className="navbar-header">
             <nav className="navbar-nav">
                 <div className="navbar-brand">
                     <a href="#">
-                        <img src="/logo_nav.png" alt="Barcia" width="150px" height="auto"></img>
+                        <img src={logo} alt="Barciaa" width="150px" height="auto"></img>
                     </a>
                 </div>
                 <ul className="navbar-links">
