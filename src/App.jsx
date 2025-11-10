d96bae3ba0e46db687ee10846bf9ce1f29a72f82
Andrés Barcia <barciaandres@gmail.com>
2025-11-10 17:42:14 -0300

    Subo entrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer.jsx'
 import ItemDetailContainer from './components/ItemDetailContainer.jsx'
+import NotFound from './components/NotFound.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<ItemListContainer />} />
         <Route path='/categories/:categoryId' element={<ItemListContainer />} />
         <Route path='/products/:productId' element={<ItemDetailContainer />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Alert } from 'react-bootstrap';
+
+const NotFound = () => {
+  return (
+    <Container className="mt-5">
+      <Alert variant="info" className="text-center">
+        <Alert.Heading>Nada por aquí!</Alert.Heading>
+        <p>
+          La página que estás buscando no existe.
+        </p>
+      </Alert>
+    </Container>
+  );
+};
+
+export default NotFound;
