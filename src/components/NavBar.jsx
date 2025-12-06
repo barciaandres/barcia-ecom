511e09553d79666093d8c9aaab2b654d3c896c8c
Andrés Barcia <barciaandres@gmail.com>
2025-12-06 12:38:55 -0300

    modifico la posicion del cartwidget en la navbar para que este siempre visible y a la derecha. antes estaba en el colapsable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,6 +27,9 @@ const NavBar = () => {
         <Navbar expand="lg" bg="dark" data-bs-theme="dark" sticky="top" className="mb-4">
             <Container fluid>
                 <Navbar.Brand as={Link} to="/">Barcia-Ecom</Navbar.Brand>
+                <Nav.Link as={Link} to="/cart" className="order-lg-last ms-auto me-2">
+                    <CartWidget />
+                </Nav.Link>
                 <Navbar.Toggle aria-controls="navbar-supported-content" />
                 <Navbar.Collapse id="navbar-supported-content">
                     <Nav className="me-auto mb-2 mb-lg-0">
@@ -40,9 +43,6 @@ const NavBar = () => {
                         </NavDropdown>
                         <Nav.Link as={Link} to="/orders">Ordenes</Nav.Link>
                     </Nav>
-                    <Nav.Link as={Link} to="/cart">
-                        <CartWidget />
-                    </Nav.Link>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
