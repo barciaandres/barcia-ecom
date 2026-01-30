40b824f56c4786b2e780ed40a77b97ea7663fcf4
barciaandres <barciaandres@gmail.com>
2026-01-29 22:09:54 -0300

    Delete src directory

--- src/context/CartContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const CartContext = createContext(null);
-
-export const useCart = () => {
-    return useContext(CartContext);
-};
-
-export default CartContext;
